dd1f9f62052b666972b34720875175e2bdcc893d
lusk <lucas78654321@outlook.com>
2024-02-01 16:38:49 -0300

    chore: added prettier plugin tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,14 +2,14 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    '@rocketseat/eslint-config/react',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    '@rocketseat/eslint-config/react'
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'prettier-plugin-tailwindcss'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
