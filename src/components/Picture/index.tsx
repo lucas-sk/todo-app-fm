0b20b986b30404bdd5e5cf6cc59b4992d7bf8056
lusk <lucas78654321@outlook.com>
2024-02-01 17:21:28 -0300

    feat: implement change picture

--- src/components/Picture/index.tsx
+++ src/components/Picture/index.tsx
@@ -1,15 +1,35 @@
+import { useTheme } from '@/context/ThemeContext'
 import bgDarkDesktop from '../../assets/bg-desktop-dark.jpg'
+import bgLightDesktop from '../../assets/bg-desktop-light.jpg'
 import bgDarkMobile from '../../assets/bg-mobile-dark.jpg'
+import bgLightMobile from '../../assets/bg-mobile-light.jpg'
 
 export const Picture = () => {
+  const { theme } = useTheme()
+
   return (
     <picture className='absolute z-0'>
-      <source media='(min-width: 376px)' srcSet={bgDarkDesktop} />
-      <img
-        src={bgDarkMobile}
-        alt='background mobile dark'
-        className='h-auto max-w-full '
-      />
+      {theme === 'dark' &&
+      <>
+        <source media='(min-width: 376px)' srcSet={bgDarkDesktop} />
+        <img
+          src={bgDarkMobile}
+          alt='background mobile dark'
+          className='h-auto max-w-full '
+        />
+      </>
+      }
+      {theme === 'light' &&
+      <>
+        <source media='(min-width: 376px)' srcSet={bgLightDesktop} />
+       <img
+         src={bgLightMobile}
+          alt='background mobile dark'
+          className='h-auto max-w-full '
+        />
+      </>
+      }
+
     </picture>
   )
 }
