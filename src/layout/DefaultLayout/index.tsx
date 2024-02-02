e742b1bc2bbc13b97bb8f0657b528b1998df44d0
lusk <lucas78654321@outlook.com>
2024-02-02 10:16:59 -0300

    styles: added max width of 2xl for main

--- src/layout/DefaultLayout/index.tsx
+++ src/layout/DefaultLayout/index.tsx
@@ -7,7 +7,7 @@ export function DefaultLayout() {
   return (
     <div className="font-sans antialiased min-h-screen text-light-900 dark:text-dark-100 bg-light-100 dark:bg-dark-900">
       <Picture />
-      <main className="mx-auto my-0 relative z-10 flex h-auto w-80 max-w-lg flex-col gap-10 pt-6">
+      <main className="mx-auto my-0 relative z-10 flex h-auto w-80 max-w-2xl flex-col gap-10 pt-6">
         <Header />
         <div className="flex flex-col gap-4">
           <TodoForm />
